d4ed4c319fec1cf3719a1f6b1e40d7a0be548234
wenderson23 <wendersonkw1@gmail.com>
2024-10-12 22:21:20 -0300

    Teste

---
+++ src/main/java/com/br/consultas/medicas/MedicasApplication.java
@@ -0,0 +1,13 @@
+package com.br.consultas.medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/consultas/medicas/MedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.consultas.medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
